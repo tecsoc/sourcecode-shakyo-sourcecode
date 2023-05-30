21413093744120b242f9f15fd95ea782b9bc35c7
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2023-05-30 22:30:53 +0900

    codemirrorのhello world

---
+++ src/app/components/CodeMirrorEditor.tsx
@@ -0,0 +1,44 @@
+"use client";
+
+import React, { useCallback, useEffect, useState } from "react";
+import {EditorView, basicSetup} from "codemirror"
+import {EditorState, Extension} from "@codemirror/state"
+import { javascript } from "@codemirror/lang-javascript";
+import styles from './CodeMirrorEditor.module.scss';
+
+function useCodeMirror(extensions: Extension[]) {
+  const [element, setElement] = useState<HTMLElement>();
+
+  const ref = useCallback((node: HTMLElement | null) => {
+    if (!node) return;
+
+    setElement(node);
+  }, []);
+
+  useEffect(() => {
+    if (!element) return;
+
+    const view = new EditorView({
+      state: EditorState.create({
+        extensions: [basicSetup, javascript(), ...extensions],
+      }),
+      parent: element,
+    });
+
+    return () => view?.destroy();
+  }, [element]);
+
+  return { ref };
+}
+
+type CodeMirrorProps = {
+  extensions: Extension[];
+};
+
+const CodeMirror = ({ extensions }: CodeMirrorProps) => {
+  const { ref } = useCodeMirror(extensions);
+
+  return <div className={styles.editor} ref={ref} />;
+};
+
+export default CodeMirror;
