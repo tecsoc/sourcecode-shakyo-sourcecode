993d67ccf8b7774a9c550268f8280493cbbd0b9a
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2023-06-03 01:10:15 +0900

    Actionsにデプロイを追加

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
+const isProduction = process.env.NODE_ENV === 'production';
 const nextConfig = {
+  assetPrefix: isProduction ? 'syakyo-typing' : '',
   output: 'export'
 };
 
