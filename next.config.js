264399955ce6375059d7224855204fadde7dd85f
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2023-06-03 01:29:05 +0900

    assetPrefixの修正

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const isProduction = process.env.NODE_ENV === 'production';
+const repository = process.env.GITHUB_REPOSITORY;
+const repositoryOwner = process.env.GITHUB_REPOSITORY_OWNER;
+const repositoryName = repository?.replace(repositoryOwner, '')
 const nextConfig = {
-  assetPrefix: isProduction ? 'syakyo-typing' : '',
+  assetPrefix: isProduction ? repositoryName : '',
   output: 'export'
 };
 
