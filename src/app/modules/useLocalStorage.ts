835ecb4c0282cf228e24934d2b8ac94b5f5d95ed
t-tecsoc <25559838+t-tecsoc@users.noreply.github.com>
2023-06-04 00:12:56 +0900

    ビルドエラーを解消

--- src/app/modules/useLocalStorage.ts
+++ src/app/modules/useLocalStorage.ts
@@ -1,11 +1,14 @@
+import isClientSide from "@/app/modules/isClientSide";
 import { useCallback, useState } from "react";
 
 const useLocalStorage = <T>(key: string, initialValue: T) => {
-  const [value, setValue] = useState(() => JSON.parse(localStorage?.getItem(key) ?? JSON.stringify(initialValue)));
+  const [value, setValue] = useState(() =>
+    JSON.parse((isClientSide() ? localStorage?.getItem(key) : null) ?? JSON.stringify(initialValue))
+  );
 
   const setLocalStorageAndSetValue = useCallback(
     (value: T) => {
-      localStorage?.setItem(key, JSON.stringify(value));
+      if (isClientSide()) localStorage.setItem(key, JSON.stringify(value));
       setValue(value);
     },
     [key]
